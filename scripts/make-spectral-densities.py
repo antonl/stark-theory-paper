651502c63053ca73ec2602ea567c7545339313b9
Anton Loukianov <aloukian@umich.edu>
2017-07-11 15:03:22 -0400

    move mode to 340 1/cm

--- scripts/make-spectral-densities.py
+++ scripts/make-spectral-densities.py
@@ -44,7 +44,7 @@ gamma_low = 0.33356
 Jlowfreq = 2*lambda_low*omega_low*gamma_low/(omega_low**2 + gamma_low**2)
 
 # add a vibrational mode to Ji
-V_mode = 150.
+V_mode = 340.
 V_strength = 0.05
 V = Ji.copy()
 V_lambda = V_mode*V_strength
